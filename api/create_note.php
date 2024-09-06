f211494e9cda9e6c28268cac71471ff3f7432e9e
oleg <beavercreek@mail.ru>
2024-09-07 00:21:15 +0700

    end

--- api/create_note.php
+++ api/create_note.php
@@ -1,9 +1,9 @@
 <?php
 //echo "<pre>";
-header('Access-Control-Allow-Origin: *');
-header('Content-type:application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-headers: Access-Control-Allow-Origin,Content-type,Access-Control-Allow-Methods,Authorization');
+// header('Access-Control-Allow-Origin: *');
+// header('Content-type:application/json');
+// header('Access-Control-Allow-Methods: POST');
+// header('Access-Control-Allow-headers: Access-Control-Allow-Origin,Content-type,Access-Control-Allow-Methods,Authorization');
 
 
 //инициализируем апи
@@ -15,7 +15,6 @@ $post =new Git($dbh);
 
 $data=json_decode(file_get_contents("php://input"));
 
-//var_dump($_POST);
 
 
 $post->name=$data->функция;
